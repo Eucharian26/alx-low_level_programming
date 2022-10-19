0505fd58c42c27708c121ff16c00fff956618eb9
Eucharian26 <eucharian26@gmail.com>
2022-10-19 13:35:22 -0700

    Jack Bauer's minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Jack_bauer - prints every minute of the day
+ * jack_bauer - prints every minute of the day
  * Description: prints every minute of the day
  * Return: void
  */
