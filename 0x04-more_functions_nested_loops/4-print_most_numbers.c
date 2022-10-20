30bc4df39c87d68035994940afa8d808e78745ec
Eucharian26 <eucharian26@gmail.com>
2022-10-20 14:45:53 -0700

    Print most numbers except 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int c = 0;
+	int c;
 
-	while (c < 1)
+	for (c = '0'; c <= '9'; c++)
 	{
 		if (c =! 2 && c =! 4)
-		_putchar(c = '0');
-
-		c++;
+			_putchar(c);
 	}
 	_putchar('\n');
 }
