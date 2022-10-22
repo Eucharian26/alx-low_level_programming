8685448f2f1d7cbaa02fcbc0109574e0a4daad8e
Eucharian26 <eucharian26@gmail.com>
2022-10-21 17:46:47 -0700

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,18 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-	for (line = 0; line <= n; line++)
-	{
-	for (gap = 0; gap <= line; gap++)
-	{
-	_putchar(' ');
-	}
-	_putchar('\\');
+		for (line = 0; line < n; line++)
+		{
+			for (gap = 0; gap < line; gap++)
+			{
+			_putchar(' ');
+			}
+		_putchar('\\');
 	if (line == n - 1)
 	{
-	continue;
+		continue;
 	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 	}
 	_putchar('\n');
