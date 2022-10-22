3b43c89db5d006efe29acf3852c31e184576c1c4
Eucharian26 <eucharian26@gmail.com>
2022-10-21 18:39:59 -0700

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (hash = 1; hash < size; has++)
+		for (hash = 1; hash <= size; has++)
 		{
 			for (index = size - hash; index > 0; index--)
 			{
