9743d3b2a88e2ea3f32beb52f37812a5c93fba49
Eucharian26 <eucharian26@gmail.com>
2022-10-21 18:29:05 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using # character.
+ * @size: The size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int has, index;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash < size; has++)
+		{
+			for (index = size - hash; index > 0; index--)
+			{
+			_putchar(' ');
+			}
+			for (index = 0; index < hash; index++)
+			{
+			_putchar('#');
+			}
+			if (hash == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
